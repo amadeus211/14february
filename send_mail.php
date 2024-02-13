b30aa4a2c9fd01fa923af876f02a4163beed8bf0
amadeus211 <malyuk2004.mail.ru@gmail.com>
2024-02-13 15:09:26 +0300

    email

--- send_mail.php
+++ send_mail.php
@@ -13,22 +13,22 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';                       //gmail SMTP server set to send through
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'user@example.com';                     //SMTP username (your gmail account)
-    $mail->Password   = 'secret';                               //SMTP password (your gmail password or app password)
+    $mail->Username   = 'malyuk2004@gmail.com';                     //SMTP username (your gmail account)
+    $mail->Password   = 'Sasha21012004';                               //SMTP password (your gmail password or app password)
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->setFrom('malyuk2004@gmail.com', 'Mailer');         //Set the sender of the message (your email address)
+    $mail->addAddress('malyuk2004.mail.ru@gmail.com', 'Joe User');     //Add a recipient (your bae's email address)
 
     //Content
     $mail->isHTML(true);
-    $mail->Subject = 'I Love You';
-    $mail->Body    = 'Happy Valentine Day My Love. I love you so much. You are the best thing that has ever happened to me ❤';
+    $mail->Subject = 'Я кохаю тебе!';
+    $mail->Body    = 'З Днем Святого Валентина, кохана. Я тебе сильно кохаю. Ти найкраще, що коли-небудь траплялося зі мною ❤.';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo '+++++';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
